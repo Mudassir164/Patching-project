e92f29d808afe124a0ca8484e2ec0b68efdbe517
Mudassir164 <mudassirahmed16164@gmail.com>
2022-08-29 13:56:35 -0700

    Feature Section created

--- src/Component/HomeScreenComponet/Components/FeatureBox.js
+++ src/Component/HomeScreenComponet/Components/FeatureBox.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { FaShirtsinbulk } from "react-icons/fa";
-// import 'featurebox.css'
+import './featurebox.css'
 
 const FeatureBox = ({icon,title,detail}) => {
   return (
-    <div className="flex flex-col justify-center items-center sm:w-[45%] md:w-[30%] p-5 bg-white  mt-10   text-orange-500  shadow-lg shadow-gray-500 feature-box">
+    <div className="flex flex-col justify-center items-center sm:w-[45%] md:w-[30%] p-8 bg-white  mt-10     shadow-lg shadow-gray-500 " id='feature-box'>
         
         {icon}
-        <h3 className='text-xl font-bold mb-5 mt-5 text-[#757575]'>{title}</h3>
+        <h3 className='text-xl font-bold mb-5 mt-5 text-[#757575] '>{title}</h3>
         <p className='text-center text-black feature-text'>{detail}</p>
     </div>
   )
--- src/Component/HomeScreenComponet/OurFeatureSection.js
+++ src/Component/HomeScreenComponet/OurFeatureSection.js
@@ -3,7 +3,7 @@ import AllFeatures from './Components/AllFeatures'
 
 const OurFeatureSection = () => {
   return (
-    <section className="w-[100%] p-15 flex flex-col  bg-[#2babe2]">
+    <section className="w-[100%] p-14 flex flex-col  bg-[#2babe2]">
         <div className='w-[100%] flex flex-col justify-center items-center '>
             <h1 className='text-2xl text-white font-bold'>Our Features</h1>
             <p className='text-lg text-white '>We deliver what we promise!</p>

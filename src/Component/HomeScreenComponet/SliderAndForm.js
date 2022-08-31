35608f9702902767bc2028bd15f9fa5ba7382d7a
Mudassir164 <mudassirahmed16164@gmail.com>
2022-08-31 04:51:28 -0700

    Some Changes for slider

--- src/Component/HomeScreenComponet/SliderAndForm.js
+++ src/Component/HomeScreenComponet/SliderAndForm.js
@@ -5,7 +5,7 @@ import Slider from "./Components/Slider";
 const SliderAndForm = () => {
   return (
     <section className="flex md:flex-row flex-col w-[100%] px-3 ">
-      <div className="w-[100%] md:w-[50%] lg:w-[60%]">
+      <div className="w-[100%] md:w-[50%] lg:w-[60%]  ">
         <Slider />
       </div>
       <div className="md:w-[50%] lg:w-[40%] w-[100%] pb-5 ">

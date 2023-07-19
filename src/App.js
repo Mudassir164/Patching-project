93e186ee08af92a7e43cc7bba40b49a775a86ae6
Mudassir Ahmed <52211617+Mudassir164@users.noreply.github.com>
2023-07-19 22:44:07 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import RegistrationScreen from "./Screens/AuthenticationScreen/RegistrationScree
 import ScrollToTop from "./Component/ScrollToTop";
 function App() {
   // HEAD
-  console.log("checking request hellooooo");
+  console.log("checking request hell0 world");
 
   return (
     <Router>
